18e2d4c4e24e22a75e0c5cdc56466622dc7fef79
Sergio Aguilar <sergioaguilar.ti@outlook.com>
2023-06-01 15:03:14 -0500

    React Redux Toolkit (RTK)

---
+++ src/components/TaskForm.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, editTask } from "../features/tasks/taskSlice";
+import { v4 as uuid } from 'uuid';
+import { useNavigate, useParams } from "react-router-dom";
+
+function TaskForm() {
+
+    const [task, setTask] = useState({
+        title: "",
+        description: ""
+    });
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const params = useParams();
+    const tasks = useSelector(state => state.tasks);
+
+    const handleChange = (e) => {
+        setTask({
+            ...task,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (params.id) {
+            dispatch(editTask(task));
+        } else {
+            dispatch(addTask({
+                ...task,
+                id: uuid(),
+                completed: false
+            }));
+        }
+
+        navigate("/");
+    };
+
+    useEffect(() => {
+        if (params.id) {
+            setTask(tasks.find(task => task.id === params.id));
+        }
+    }, []);
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                name="title"
+                type="text"
+                placeholder="title"
+                onChange={handleChange}
+                value={task.title} />
+            <textarea
+                name="description"
+                placeholder="description"
+                onChange={handleChange}
+                value={task.description} />
+            <button>Save</button>
+        </form>
+    );
+}
+
+export default TaskForm;
