03e94d785f7c7f0baca276d6a2f079ad2ee33592
Sergio Aguilar <sergioaguilar.ti@outlook.com>
2023-06-01 15:57:00 -0500

    css de tailwind

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -43,22 +43,26 @@ function TaskForm() {
         if (params.id) {
             setTask(tasks.find(task => task.id === params.id));
         }
-    }, []);
+    }, [params.id, tasks]);
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="bg-zinc-800 mx-w-sm p-4 mb-1">
+            <label htmlFor="title" className="block text-xs font-bold mb-2">Task: </label>
             <input
                 name="title"
                 type="text"
                 placeholder="title"
                 onChange={handleChange}
-                value={task.title} />
+                value={task.title} 
+                className="w-full p-2 rounded-md bg-zinc-600 mb-2"/>
+            <label htmlFor="description" className="block text-xs font-bold mb-2">Description: </label>
             <textarea
                 name="description"
                 placeholder="description"
                 onChange={handleChange}
-                value={task.description} />
-            <button>Save</button>
+                value={task.description} 
+                className="w-full p-2 rounded-md bg-zinc-600 mb-2"/>
+            <button className="bg-indigo-600 px-2 py-1">Save</button>
         </form>
     );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -14,23 +14,34 @@ function TaskList() {
     }
 
     return (
-        <div>
-            <header>
+        <div className="w-4/6">
+            <header className="flex justify-between items-center py-4">
                 <h1>Tasks {tasks.length}</h1>
-                <Link to="/create-task">
+                <Link to="/create-task" className="bg-indigo-600 px-2 py-1 rounded-sm text-sm">
                     Create Tak
                 </Link>
             </header>
+            <div className="grid grid-cols-3 gap-4">
             {
                 tasks.map(task => (
-                    <div key={task.id}>
-                        <h3>{task.title}</h3>
+                    <div key={task.id} className="bg-neutral-800 p-4 rounded-md">
+                        <header className="flex  justify-between">
+                            <h3>{task.title}</h3>
+                            <div className="flex gap-x-2">
+                                <Link to={`/edit-task/${task.id}`}
+                                    className="bg-zinc-600 px-2 py-1 text-xs rounded-md">Edit</Link>
+                                
+                                <button onClick={() => handleDelete(task.id)}
+                                    className="bg-red-500 px-2 py-1 text-xs rounded-md self-center"
+                                >Delete</button>
+                            </div>
+                        </header>
                         <p>{task.description}</p>
-                        <button onClick={() => handleDelete(task.id)}>Delete</button>
-                        <Link to={`/edit-task/${task.id}`}>Edit</Link>
+                        
                     </div>
                 ))
             }
+            </div>
         </div>
     )
 }
